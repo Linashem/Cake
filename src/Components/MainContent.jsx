7fade099095e135d9670bea7ea9a37d06cb5cfe0
Linashem <lina.shemeneva@yandex.ru>
2023-10-03 23:18:48 +0300

    befor preview module

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Carousel } from "./Carousel/Carousel";
 import { Goods } from "./Goods/Goods";
+import { Preview } from "./Preview/Preview";
 
 export const MainContent = () => {
   return (
     <>
       {/* <Goods/> */}
       <Carousel />
+      <Preview/>
     </>
   );
 };

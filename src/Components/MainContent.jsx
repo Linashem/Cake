41ad4e1e23d906a91e60afce0d3002a80a1af950
Linashem <lina.shemeneva@yandex.ru>
2023-10-07 15:15:00 +0300

    coments start

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Carousel } from "./Carousel/Carousel";
+import { Comments } from "./Comments/Comments";
 import { NewTastes } from "./NewTastes/NewTastes";
 
 export const MainContent = () => {
   return (
     <>
-      <Carousel />
-      <NewTastes/>
+      {/* <Carousel /> */}
+      {/* <NewTastes/> */}
+      <Comments/>
     </>
   );
 };
